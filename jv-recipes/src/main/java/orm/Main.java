1a44535118f2f4f218146e880c4078f23cbf0a81
Ainkharan <ainkharansoftdev@gmail.com>
2024-06-04 11:14:33 +0100

    task-3 completed

---
+++ jv-recipes/src/main/java/orm/Main.java
@@ -0,0 +1,13 @@
+package orm;
+
+
+import org.hibernate.SessionFactory;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Database.getSessionFactory();
+
+
+        }
+    }
