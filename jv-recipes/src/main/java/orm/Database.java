0a36c00133e4f84f6388f8221dab1154fab628c7
tomfegan <tomfegan83@gmail.com>
2024-06-04 12:29:02 +0100

    update:tasks 4-5 completed. task 6 started

--- jv-recipes/src/main/java/orm/Database.java
+++ jv-recipes/src/main/java/orm/Database.java
@@ -1,25 +1,32 @@
 package orm;
+
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
 import static java.lang.Boolean.TRUE;
 import static org.hibernate.cfg.JdbcSettings.*;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 
 public class Database {
+
+
+
     public static SessionFactory getSessionFactory(){
-        // TODO
+
         var sessionFactory = new Configuration()
+                .setProperty("hibernate.agroal.maxSize", "20")
                 // use H2 in-memory database
                 .setProperty(URL, "jdbc:h2:mem:db1")
                 // default username / password
-                .setProperty(USER, "sa")
+                .setProperty(USER, "sa") // sa = server administrator
                 .setProperty(PASS, "")
                 // options to display SQL in console
                 .setProperty(SHOW_SQL, TRUE.toString())
                 .setProperty(FORMAT_SQL, TRUE.toString())
                 .setProperty(HIGHLIGHT_SQL, TRUE.toString())
                 .buildSessionFactory();
+
+
+
 
         return sessionFactory;
     }
--- jv-recipes/src/main/java/orm/Main.java
+++ jv-recipes/src/main/java/orm/Main.java
@@ -1,13 +1,13 @@
 package orm;
 
 
-import org.hibernate.SessionFactory;
-
 public class Main {
     public static void main(String[] args) {
 
-        Database.getSessionFactory();
+        Database.getSessionFactory()
+                .getSchemaManager() // A SchemaManager with the same default catalog and schema as pooled connections belonging to this factory. Intended mostly as a convenience for writing tests.
+                .exportMappedObjects(true); // exports database objects mapped by Hibernate entities. If true, attempt to create schemas, otherwise, assume the schemas already exist
 
 
-        }
     }
+}
